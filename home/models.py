6f9dc050559b17f6d0e9adc9af623d76655ce035
Diogo Campos <diogotc2207@gmail.com>
2026-09-23 10:42:08 -0300

    Fixing models.py

--- home/models.py
+++ home/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 #tabela Mensagem com tres colunas
-class Mensagem(model.Model):
+class Mensagem(models.Model):
     titulo = modelos.CharField(max_length=120)
     conteudo = models.TextField()
     criada_em = models.DateTimeField(auto_now_add=True) #preenche a data
