f3684bc6ccdde20129046e57627f42a5b042b0d4
Diogo Campos <diogotc2207@gmail.com>
2026-09-23 10:26:10 -0300

    Inicio do projeto django

---
+++ home/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+#tabela Mensagem com tres colunas
+class Mensagem(model.Model):
+    titulo = modelos.CharField(max_length=120)
+    conteudo = models.TextField()
+    criada_em = models.DateTimeField(auto_now_add=True) #preenche a data
+
+    class Meta:
+        ordering = ["-criada_em"]
+
+    def __str__(self):
+        return self.titulo
